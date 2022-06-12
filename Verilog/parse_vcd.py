2b61ec63a201c77bd75c1f937d9865860dc9e5ec
msiddalingaiah <madhu@madhu.com>
2022-06-12 00:19:38 -0400

    More detailed ucode dis

--- Verilog/parse_vcd.py
+++ Verilog/parse_vcd.py
@@ -108,10 +108,10 @@ class VCDFile(object):
         return result
 
 e6Map = {0:'', 1:'RR<-FBus', 2:'RI<-FBus', 3:'', 4:'', 5:'MAR<->WAR', 6:'', 7:'LoadCC'}
-k11Map = {0:'', 1:'', 2:'', 3:'Load F11', 4:'', 5:'', 6:'WAR.LO<-', 7:'WrBus'}
+k11Map = {0:'', 1:'', 2:'', 3:'Load F11', 4:'R[]<-', 5:'', 6:'WAR.LO<-', 7:'WrBus'}
 pcIncMap = {0:'', 1:'PC++'}
-d2d3Map = {0:'DPBus=Swap', 1:'DPBus=Reg', 2:'DPBus=MAR.HI', 3:'DPBus=MAR.LO', 4:'', 5:'', 6:'', 7:'', 8:'',
-            9:'DPBus=CC', 10:'DPBus=BusIn', 11:'', 12:'', 13:'DPBus=const', 14:'', 15:''}
+d2d3Map = {0:'D=Swap', 1:'D=Reg', 2:'D=MAR.HI', 3:'D=MAR.LO', 4:'', 5:'', 6:'', 7:'', 8:'',
+            9:'D=CC', 10:'D=BusIn', 11:'', 12:'', 13:'D=const', 14:'', 15:''}
 
 ALU_SRC_MAP = [['A', 'Q'], ['A', 'B'], ['0', 'Q'], ['0', 'B'], ['0', 'A'], ['D', 'A'], ['D', 'Q'], ['D', '0']]
 ALU_OP_MAP = ['{r}+{s}', '{s}-{r}', '{r}-{s}', '{r}|{s}', '{r}&{s}', '(~{r})&{s}', '{r}^{s}', '~({r}^{s})']
@@ -129,14 +129,6 @@ class Disassembler(object):
         tag = self.signalTagMap[name]
         return sig[tag]
 
-    def disassembleAll(self):
-        i = 1
-        while i < len(self.signals[1:]):
-            sig = self.signals[i]
-            i += 1
-            if self.getSignal(sig, 'cg0.clock').value == 1:
-                print(self.disassembleOne(sig))
-
     def getALUCode(self, aluA, aluB, aluOp, aluSrc, aluDest, cin):
         r, s = ALU_SRC_MAP[aluSrc]
         if r == 'A':
@@ -152,7 +144,22 @@ class Disassembler(object):
         q = ALU_Q_DEST_MAP[aluDest].format(f=f)
         a = f'r{aluA}'
         y = ALU_OUT_MAP[aluDest].format(f=f, a=a)
-        return f'{mem}+{cin} {q} {y}'
+        if (aluOp == 0 or aluOp == 1) and cin == 1:
+            return f'{mem}+{cin} {q} {y}'
+        return f'{mem} {q} {y}'
+
+    def disassembleAll(self):
+        code = []
+        i = 0
+        while i < len(self.signals):
+            sig = self.signals[i]
+            i += 1
+            if self.getSignal(sig, 'cg0.clock').value == 1:
+                instruction_start = self.getSignal(sig, 'cpu.instruction_start').value
+                if instruction_start == 1:
+                    code.append('')
+                code.append(self.disassembleOne(sig))
+        return code
 
     def disassembleOne(self, sig):
         clock = self.getSignal(sig, 'cg0.clock')
@@ -163,6 +170,15 @@ class Disassembler(object):
         pcInc = self.getSignal(sig, 'cpu.pc_increment').value
         d2d3 = self.getSignal(sig, 'cpu.d2d3').value
         constant = self.getSignal(sig, 'cpu.constant').value
+        dataInBus = self.getSignal(sig, 'cpu.dataInBus').value
+        map_rom_data = self.getSignal(sig, 'cpu.map_rom_data').value
+        reg_ram_data_out = self.getSignal(sig, 'cpu.reg_ram_data_out').value
+        register_index = self.getSignal(sig, 'cpu.register_index').value
+        register_index = self.getSignal(sig, 'cpu.register_index').value
+        FBus = self.getSignal(sig, 'cpu.FBus').value
+        memory_address = self.getSignal(sig, 'cpu.memory_address').value
+        result_register = self.getSignal(sig, 'cpu.result_register').value
+
         if k11 == 6:
             k11Map[6] = 'WAR.LO<-RR'
             if e6 == 5:
@@ -171,11 +187,23 @@ class Disassembler(object):
             k11Map[6] = 'WAR.HI<-RR'
             if e6 == 5:
                 k11Map[6] = 'WAR.HI<-MAR.HI'
+        if d2d3 == 1:
+            d2d3Map[1] = f'D=R[{register_index:02x}]({reg_ram_data_out:02x})'
+        if d2d3 == 10:
+            d2d3Map[10] = f'D=Bus({dataInBus:02x})'
         if d2d3 == 13:
-            d2d3Map[13] = f'DPBus<-{constant:02x}'
-        fbus = 'FBus<-Y'
+            d2d3Map[13] = f'D={constant:02x}'
+        fbr = 'FBus=Y'
         if h11 == 6:
-            fbus = 'FBus<-MAP'
+            fbr = f'FBus=Map({map_rom_data:02x})'
+        if e6 == 1:
+            e6Map[1] = f'RR<-FBus({FBus:02x})'
+        if e6 == 2:
+            e6Map[2] = f'RI<-FBus({FBus:02x})'
+        if pcInc == 1:
+            pcIncMap[1] = f'PC++({memory_address:04x})'
+        if k11 == 4:
+            k11Map[4] = f'R[{register_index:02x}]<-RR({result_register:02x})'
         alu_a = self.getSignal(sig, 'cpu.alu_a').value
         alu_b = self.getSignal(sig, 'cpu.alu_b').value
         alu_src = self.getSignal(sig, 'cpu.alu_src').value
@@ -184,11 +212,12 @@ class Disassembler(object):
         alu0_cin = self.getSignal(sig, 'cpu.alu0_cin').value
         aluCode = self.getALUCode(alu_a, alu_b, alu_op, alu_src, alu_dest, alu0_cin)
 
-        return f'{int(clock.time/100)} {addr:03x}: {d2d3Map[d2d3]} {aluCode} {fbus} _|- {e6Map[e6]} {k11Map[k11]} {pcIncMap[pcInc]}'
+        return f'{int(clock.time/100)} {addr:03x}: {d2d3Map[d2d3]} {aluCode} {fbr}  /\\  {e6Map[e6]} {k11Map[k11]} {pcIncMap[pcInc]}'
 
 if __name__ == '__main__':
     vcd = VCDFile('vcd/CPUTestBench.vcd')
     dis = Disassembler(vcd.signals, vcd.signalTagMap)
-    dis.disassembleAll()
-    #x = [f"{i}:''" for i in range(16)]
-    #print(', '.join(x))
+    code = dis.disassembleAll()
+    with open('vcd/CPUTestBench.txt', 'wt') as f:
+        for c in code:
+            f.write(f'{c}\n')
